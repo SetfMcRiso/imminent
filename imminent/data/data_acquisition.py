1b05c925a94afa3247cff796985202b5027527c3
Stefanos Makrysopoulos <stef.makryso@gmail.com>
2020-08-31 16:33:28 +0200

    added requests to handle the curl commands

--- imminent/data/data_acquisition.py
+++ imminent/data/data_acquisition.py
@@ -2,8 +2,11 @@ import subprocess
 import json
 import os
 import ast
+from pprint import pprint
 from pathlib import Path
-from imminent.setting_handling.setting_handling import JSON
+from imminent.setting.setting_handling import JSON
+from requests_oauthlib import OAuth2Session
+import requests
 
 
 class Data():
@@ -11,27 +14,35 @@ class Data():
     Class that is handling the data acquisition.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, name=None, realm=None):
+        self.name = name
+        self.realm = realm
 
-    def _execute_command(self, cmd, cwd=None):
-        self._last_cmd = cmd
-        try:
-            out = subprocess.check_output(self._last_cmd, cwd=cwd,
-                                          shell=True, universal_newlines=True,
-                                          stderr=subprocess.STDOUT)
-            return out
-        except subprocess.CalledProcessError as ex:
-            self._error(
-                "Fail {}/\n when calling command '{}'.".format(cmd, ex))
-            return -1
+    def get_access_token(self):
+        client_id = self._get_client_id()
+        client_secret = self._get_client_secret()
+        token_url = 'https://us.battle.net/oauth/token'
+        grant_type = 'client_credentials'
+        body_params = {'grant_type': grant_type}
+        response = requests.post(token_url, data=body_params,
+                                 auth=(client_id, client_secret))
+        token_raw = json.loads(response.text)
+        print(token_raw)
+        return token_raw["access_token"]
+
+    def _get_client_id(self):
+        return '966c2753943b44988942d3faf60db027'
+
+    def _get_client_secret(self):
+        return 'WCSEYp0Ks75J7xDtoIHHgCX8wkS1D4cX'
 
 
 if __name__ == "__main__":
-    out = Data()._execute_command('curl -H "Authorization: Bearer USRVbibiAyyHQHSbjqa7Cafn66ho8fJ0A0" https://eu.api.blizzard.com/profile/wow/character/twisting-nether/kugarina/character-media?namespace=profile-eu')
-    mitsos = ast.literal_eval(out.splitlines()[-1])
-    mitsaras = JSON(os.path.join(Path(__file__).parents[0], 'test.json'))
-    mitsaras.values = mitsos
-    mitsaras.save_setting()
-    print(mitsos)
-    print(type(mitsos))
+    token = Data().get_access_token()
+    print(token)
+    headers = {"Authorization": "Bearer {}".format(token)}
+    r = requests.get(
+        url="https://eu.api.blizzard.com/profile/wow/character/twisting-nether/kugarina/equipment?namespace=profile-eu&locale=en_GB", headers=headers)
+    resp = json.loads(r.text)
+    pprint(resp)
+    print(len(resp))
